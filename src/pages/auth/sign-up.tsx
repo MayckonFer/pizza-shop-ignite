a1f6c5ffb38699e50543850c48e8e2ce0d654dd8
MayckonFer <mayckonfernandes1@gmail.com>
2024-08-21 08:09:46 -0300

    arrumando bug

--- src/pages/auth/sign-up.tsx
+++ src/pages/auth/sign-up.tsx
@@ -1,110 +1,112 @@
-import { Helmet } from "react-helmet-async"
-import { useForm } from 'react-hook-form'
-import { z } from 'zod'
-import { Link, useNavigate } from "react-router-dom"
-import { toast } from 'sonner'
+import { Helmet } from "react-helmet-async";
+import { useForm } from "react-hook-form";
+import { z } from "zod";
+import { Link, useNavigate } from "react-router-dom";
+import { toast } from "sonner";
 
-import { Button } from "@/components/ui/button"
-import { Input } from "@/components/ui/input"
-import { Label } from "@/components/ui/label"
+import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
+import { Label } from "@/components/ui/label";
 
 const signUpForm = z.object({
   restaurantName: z.string(),
   manageName: z.string(),
   phone: z.string(),
-  email: z.string().email()
-})
+  email: z.string().email(),
+});
 
-type SingInForm = z.infer<typeof signUpForm>
+type SingInForm = z.infer<typeof signUpForm>;
 
 export function SignUp() {
-  const navigate = useNavigate()
+  const navigate = useNavigate();
 
-  const { 
-    register, 
-    handleSubmit, 
-    formState: { isSubmitting } 
-  } = useForm<SingInForm>()
+  const {
+    register,
+    handleSubmit,
+    formState: { isSubmitting },
+  } = useForm<SingInForm>();
 
   async function handleSingIn(data: SingInForm) {
-    try {
-      await new Promise((resolve) => setTimeout(resolve, 2000))
+    console.log(data);
 
-      toast.success('Restaurante cadastrado com sucesso!', {
+    try {
+      await new Promise((resolve) => setTimeout(resolve, 2000));
+
+      toast.success("Restaurante cadastrado com sucesso!", {
         action: {
           label: "Login",
-          onClick: () => navigate('/sign-in')
-        }
-      })
+          onClick: () => navigate("/sign-in"),
+        },
+      });
     } catch {
-      toast.success('Erro ao cadastrar restaurante.')
+      toast.success("Erro ao cadastrar restaurante.");
     }
-  } 
+  }
 
   return (
     <>
-      <Helmet title="Cadastro"/>
+      <Helmet title="Cadastro" />
       <div className="p-8 ">
-      <Button variant="ghost" asChild className="absolute right-8 top-8">
-          <Link to="/sign-in">
-            Fazer login
-          </Link>
+        <Button variant="ghost" asChild className="absolute right-8 top-8">
+          <Link to="/sign-in">Fazer login</Link>
         </Button>
 
-        <div className="w-[350px] flex flex-col justify-center gap-6">
+        <div className="flex w-[350px] flex-col justify-center gap-6">
           <div className="flex flex-col gap-2 text-center">
-            <h1 className="text-2xl font-semibold tracking-tight">Criar conta grátis</h1>
-            <p className="text-sm text-muted-foreground">Seja um parceiro e comece suas vendas!</p>
+            <h1 className="text-2xl font-semibold tracking-tight">
+              Criar conta grátis
+            </h1>
+            <p className="text-sm text-muted-foreground">
+              Seja um parceiro e comece suas vendas!
+            </p>
           </div>
 
           <form onSubmit={handleSubmit(handleSingIn)} className="space-y-4">
             <div className="space-y-2">
               <Label htmlFor="restaurantName">Nome do estabelecimento</Label>
 
-              <Input 
-                id="restaurantName" 
-                type="text" 
-                {...register('restaurantName')}
+              <Input
+                id="restaurantName"
+                type="text"
+                {...register("restaurantName")}
               />
             </div>
 
             <div className="space-y-2">
               <Label htmlFor="manageName">Seu nome</Label>
 
-              <Input 
-                id="manageName" 
-                type="text" 
-                {...register('manageName')} 
-              />
+              <Input id="manageName" type="text" {...register("manageName")} />
             </div>
 
             <div className="space-y-2">
               <Label htmlFor="email">Seu e-mail</Label>
 
-              <Input 
-                id="email" 
-                type="email" 
-                {...register('email')} 
-              />
+              <Input id="email" type="email" {...register("email")} />
             </div>
 
             <div className="space-y-2">
               <Label htmlFor="phone">Seu celular</Label>
 
-              <Input 
-                id="phone" 
-                type="tel" 
-                {...register('phone')} 
-              />
+              <Input id="phone" type="tel" {...register("phone")} />
             </div>
 
-            <Button disabled={isSubmitting} type="submit" className="w-full">Finalizar cadastro</Button>
+            <Button disabled={isSubmitting} type="submit" className="w-full">
+              Finalizar cadastro
+            </Button>
             <p className="px-6 text-center text-sm leading-relaxed text-muted-foreground">
-              Ao continuar você concorda com nossos <a href="" className="underline underline-offset-4">termos de serviço</a> e <a href="" className="underline underline-offset-4">políticas de privacidade</a>.
+              Ao continuar você concorda com nossos{" "}
+              <a href="" className="underline underline-offset-4">
+                termos de serviço
+              </a>{" "}
+              e{" "}
+              <a href="" className="underline underline-offset-4">
+                políticas de privacidade
+              </a>
+              .
             </p>
           </form>
         </div>
       </div>
     </>
-  )
+  );
 }
